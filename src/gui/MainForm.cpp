0da6dbbdb91003325efced7a625347211aa95bc5
Sam Fredrickson <kinghajj@gmail.com>
2008-11-28 15:58:00 -0800

    Deleted everything to merge the C++ branch.

--- src/gui/MainForm.cpp
+++
@@ -1,7 +0,0 @@
-#include "gui/MainForm.h"
-
-MainForm::MainForm(const wxString& title)
-         : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
-{
-	Centre();
-}
--- src/include/gui/App.h
+++
@@ -1,7 +0,0 @@
-#include <wx/wx.h>
-
-class App : public wxApp
-{
-public:
-	virtual bool OnInit();
-};
